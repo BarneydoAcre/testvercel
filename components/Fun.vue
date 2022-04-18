919c1a5e175fbd287c9f9658503264c6e4755f36
unknown <gabrielfauth@outlook.com>
2022-04-17 22:30:32 -0400

    Test Nuxt

---
+++ components/Fun.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1 class="red">Hello {{ slug }}!</h1>
+</template>
+
+<script>
+  export default {
+      name: 'Fun',
+      data () {
+          return {
+              slug: 'teste'
+          }
+      }
+  }
+</script>
+
+<style>
+  .red {
+    color: red;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div> 
+    <Fun />
+    <Tutorial/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  components: {
+  }
+}
+</script>
